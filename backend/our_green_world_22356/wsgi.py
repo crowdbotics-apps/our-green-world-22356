dcedc6f77b5be635671a795534da32677c1f623e
Crowdbotics <team@crowdbotics.com>
2020-11-06 18:14:32 +0000

    Update models

--- backend/our_green_world_22356/wsgi.py
+++ backend/our_green_world_22356/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'our_green_world_22356.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "our_green_world_22356.settings")
 
 application = get_wsgi_application()
